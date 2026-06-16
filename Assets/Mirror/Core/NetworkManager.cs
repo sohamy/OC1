4bc300a0ce3ce5befad86d8ff47b86f168cb8ce8
sohamy <61020364+sohamy@users.noreply.github.com>
2026-06-16 17:33:17 +0900

    ip 수정

--- Assets/Mirror/Core/NetworkManager.cs
+++ Assets/Mirror/Core/NetworkManager.cs
@@ -71,7 +71,7 @@ namespace Mirror
         /// <summary>Server's address for clients to connect to.</summary>
         [FormerlySerializedAs("m_NetworkAddress")]
         [Tooltip("Network Address where the client should connect to the server. Server does not use this for anything.")]
-        public string networkAddress = "localhost";
+        public string networkAddress = "20.196.155.205";
 
         /// <summary>The maximum number of concurrent network connections to support.</summary>
         [FormerlySerializedAs("m_MaxConnections")]
